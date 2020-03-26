32b12a27c8cf18a36680e237940743da48a778b9
Ken Garbutt <kgarbutt@gmail.com>
2020-03-26 16:28:27 -0700

    Prints a greeting (after the user enters a nam)e and a random  number.

---
+++ greeting.py
@@ -0,0 +1,9 @@
+
+import random
+
+def greeting():
+	name = input("Hello!, What's your name?")
+	number = random.randint(1,101)
+	print("hello " + name + ", your random number is " + str(number))
+
+greeting()
